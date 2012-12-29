f1ab82f9d5936e121e5c2ac1b78cb9b26852015b
Andreas Ek <andreas@flowcom.se>
2012-12-29 14:22:38 +0100

    Main info updated

--- plugins/api/main.php
+++ plugins/api/main.php
@@ -13,7 +13,7 @@ $url = PHPlist_API_Common::apiUrl( $website );
 
     <h2>Version 0.2.3</h2>
     <p>The plugin provides a REST API to PHPlist.<br/>
-    Development by Flowcom AB, Andreas Ek (<a href="">@EkAndreas</a>)</p>
+    Development by Flowcom AB, Andreas Ek (<a href="https://twitter.com/ekandreas">@EkAndreas</a>)</p>
 
     <p>
         <h2>Commands</h2>
@@ -29,7 +29,7 @@ $url = PHPlist_API_Common::apiUrl( $website );
         <a href="<?php echo $url; ?>"><?php echo $url; ?></a>
     </p>
     <p>
-        First login to PHPlist with method /post and body parameters "login" and "password". Your session is then cleared!<br/>
+        First login to PHPlist with method POST and body parameters "login" and "password".<br/>
     </p>
     <p>
         <h2>Client</h2>
@@ -39,80 +39,15 @@ $url = PHPlist_API_Common::apiUrl( $website );
     </p>
 
     <p>
-        <h2>Standard reponse</h2>
-        All responses is returned in json and encoded to UTF-8.<br/>
-        <br/>
-        Success response from the API<br/>
-        {
-        <blockquote>
-            "status":"success",<br/>
-            "type":"List",<br/>
-            "data":[{
-            <blockquote>
-                "id":"12",<br/>
-                "name":"A new list in PHPlist"
-            </blockquote>
-            }]
-        </blockquote>
-        }<br/>
-        <br/>
-        Error response from the API<br/>
-        {
-        <blockquote>
-            "status":"error",<br/>
-            "type":"Error",<br/>
-            "data":[{
-            <blockquote>
-                "code":"123",<br/>
-                "message":"Error when creating list"
-            </blockquote>
-            }]
-        </blockquote>
-        }<br/>
-        <br/>
+        <h2>More information</h2>
+        Please read more at Github!
+        <a href="https://github.com/EkAndreas/phplistapi">https://github.com/EkAndreas/phplistapi</a>
     </p>
 
     <p>
-        <h2>Example in PHP:</h2>
-        <span style="font-weight:bold;color:green">// Copy and paste this code-example!</span><br/><br/>
-        <span style="font-weight:bold;color:green">//Get the session via login</span><br/><br/>
-        <span style="font-weight:bold;color:green">//initialize cUrl for remote content</span><br/>
-        $c = curl_init();<br/>
-        curl_setopt($c, CURLOPT_COOKIEFILE,     'PHPlist_API_Helper');
-        curl_setopt($c, CURLOPT_COOKIEJAR,      'PHPlist_API_Helper');
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);<br/>
-        curl_setopt($c, CURLOPT_POST,           1);<br/>
-        <br/>
-        <span style="font-weight:bold;color:green">//Call for the session-id via /login</span>
-        <br/>
-        curl_setopt($c, CURLOPT_URL,            '<?php echo $url; ?>' );<br/>
-        curl_setopt($c, CURLOPT_POSTFIELDS,     'cmd=login&login=admin&password=admin');<br/>
-        $result = curl_exec($c);<br/>
-        $result = json_decode($result);<br/>
-        var_dump( $result->data );
-        <br/>
-        <br/>
-        <span style="font-weight:bold;color:green">//Get all lists in PHPlist via /listsGet</span>
-        <br/>
-        curl_setopt($c, CURLOPT_URL,            '<?php echo $url; ?>' );<br/>
-        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);<br/>
-        curl_setopt($c, CURLOPT_POST,           1);<br/>
-        curl_setopt($c, CURLOPT_POSTFIELDS,     'cmd=listsGet' );<br/>
-        $result = curl_exec($c);<br/>
-        $result = json_decode($result);<br/>
-        <br/>
-        <span style="font-weight:bold;color:green">//Now close the cUrl when finished</span>
-        <br/>
-        curl_close($c);<br/>
-        <br/>
-        <span style="font-weight:bold;color:green">//Dump all lists in PHPlist via /listsGet</span>
-        <br/>
-        var_dump($result->data);<br/>
-
-    </p>
-
-    <p>
-        &nbsp;
+        <h2>Issues</h2>
+        All issues regarding the API is handled at Github!
+        <a href="https://github.com/EkAndreas/phplistapi">https://github.com/EkAndreas/phplistapi</a>
     </p>
 
 <?php
