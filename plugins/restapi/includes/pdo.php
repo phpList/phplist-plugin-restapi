69a3d8f79eb7910a516b83027876881bab6c9374
Michiel Dethmers <git@phplist.com>
2015-11-24 17:51:06 +0000

    initialise with utf8

--- plugins/restapi/includes/pdo.php
+++ plugins/restapi/includes/pdo.php
@@ -15,7 +15,7 @@ class PDO extends \PDO
         $dbuser = $GLOBALS['database_user'];
         $dbpass = $GLOBALS['database_password'];
         $dbname = $GLOBALS['database_name'];
-        $dbh = new \PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+        $dbh = new \PDO("mysql:host=$dbhost;dbname=$dbname;charset=UTF8;", $dbuser, $dbpass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8';"));
         $dbh->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
         return $dbh;
